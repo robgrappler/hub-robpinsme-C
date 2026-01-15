f85d73b716613c1413797d0213a82fbb1bae6918
robgrappler <robgrapsk@gmail.com>
2026-01-15 02:27:51 -0600

    feat: implement Spanish localization (i18n), language picker, and localized pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,12 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://hub.robpins.me',
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   integrations: [tailwind({ applyBaseStyles: false })]
 });
